245c42141435708730dbd6dc74eb938cb28c3eb0
abdooman21 <abdos3new55@gmail.com>
2026-08-17 03:50:47 +0300

    setup repo as abdooman21

--- handler_video_meta.go
+++ handler_video_meta.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
-	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
+	"github.com/abdooman21/file-storage-s3-golang/internal/auth"
+	"github.com/abdooman21/file-storage-s3-golang/internal/database"
 	"github.com/google/uuid"
 )
 
